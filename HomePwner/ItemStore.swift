86df7d3aaedac5d497a19f0d5e9a29a3758f9f1d
Tyler Percy <tpercy@ironman.highpoint.edu>
2019-03-18 13:25:52 -0400

    Chapter 10-11 done, Bronze Challenges Done, Silver In Progress

---
+++ HomePwner/ItemStore.swift
@@ -0,0 +1,46 @@
+//
+//  ItemStore.swift
+//  HomePwner
+//
+//  Created by Tyler Percy on 3/18/19.
+//  Copyright © 2019 Tyler Percy. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        
+        return newItem
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
+    
+    init() {
+        let noMoreItems = Item(random: false)
+        noMoreItems.name = "No More Items!"
+        allItems.append(noMoreItems)
+    }
+}
+
+
